575f6e0faf80b38071cb269f85347a597d179de3
Bruno Mikoski <badawe@gmail.com>
2021-09-15 14:56:01 +0100

    add: default refresh on open enabled

--- Scripts/Runtime/Core/DebugPanel.cs
+++ Scripts/Runtime/Core/DebugPanel.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using Object = UnityEngine.Object;
 
 namespace BrunoMikoski.DebugTools.Core
 {
