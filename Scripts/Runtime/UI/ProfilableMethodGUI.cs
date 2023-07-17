93404869f5d37fedb3eeed26bee368000cb80221
Bruno Mikoski <bruno.mikoski@bossastudios.com>
2023-07-17 14:16:11 +0100

    fix: namespace

--- Scripts/Runtime/UI/ProfilableMethodGUI.cs
+++ Scripts/Runtime/UI/ProfilableMethodGUI.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using BrunoMikoski.Core;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
--- Scripts/Runtime/Utils/PerformanceTester.cs
+++ Scripts/Runtime/Utils/PerformanceTester.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace BrunoMikoski.Core
+namespace BrunoMikoski.DebugTools
 {
     public static class PerformanceTester
     {
